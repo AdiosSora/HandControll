367d7110f3295eb30af9260ec3d4cfbaa1fa9224
S3A1 Student <S3A1@KCS-Learning.local>
2020-11-04 10:05:30 +0900

    AddPose翻訳

--- AddPose.py
+++ AddPose.py
@@ -21,7 +21,8 @@ def main():
         print('Enter a name for the pose you want to add :')
         name_pose = input()
 
-        # Create folder for pose 
+        # Create folder for pose
+        #ポーズ用のフォルダを作成する
         if not os.path.exists('Poses/' + name_pose):
             os.makedirs('Poses/' + name_pose + '/' + name_pose + '_1/')
             currentPath = 'Poses/' + name_pose + '/' + name_pose + '_1/'
@@ -29,6 +30,7 @@ def main():
 
     elif(menu_choice == '2'):
         # Display current poses
+        #現在のポーズを表示する
         dirs = os.listdir('Poses/')
         dirs_choice = ''
         possible_choices = []
@@ -37,30 +39,35 @@ def main():
             dirs_choice += str(i) + ' - ' + str(_dir) + ' / '
             possible_choices.append(str(i))
             i+=1
-        
+
         # Ask user to choose to which pose to add examples
+        #例を追加するポーズを選択するようにユーザーに依頼します
         print('Choose one of the following pose:')
-        print(dirs_choice)       
+        print(dirs_choice)
         choice = input()
         while(not choice in possible_choices and dirs[int(choice)-1]=='garbage'):
             print('Please enter one of the following (not garbage): ' + str(possible_choices))
             choice = input()
 
         # Count number of files to increment new example directory
+        #新しいサンプルディレクトリをインクリメントするファイルの数を数える
         subdirs = os.listdir('Poses/' + dirs[int(choice)-1] + '/')
         index = len(subdirs) + 1
 
         # Create new example directory
+        #新しいサンプルディレクトリを作成します
         if not os.path.exists('Poses/' + dirs[int(choice)-1] + '/' + dirs[int(choice)-1] + '_' + str(index) + '/'):
             os.makedirs('Poses/' + dirs[int(choice)-1] + '/' + dirs[int(choice)-1] + '_' + str(index) + '/')
 
             #Update current path
+            #現在のパスを更新する
             currentPath = 'Poses/' + dirs[int(choice)-1] + '/' + dirs[int(choice)-1] + '_' + str(index) + '/'
             currentExample = dirs[int(choice)-1] + '_' + str(index) + '_'
             name_pose = dirs[int(choice) - 1]
-    
+
     elif(menu_choice == '3'):
-        # Create folder for pose 
+        # Create folder for pose
+        #ポーズ用のフォルダを作成する
         if not os.path.exists('Poses/Garbage/'):
             os.makedirs('Poses/Garbage/Garbage_1/')
             currentPath = 'Poses/Garbage/Garbage_1/'
@@ -68,17 +75,20 @@ def main():
             name_pose = 'Garbage'
         else:
             # Count number of files to increment new example directory
+            #新しいサンプルディレクトリをインクリメントするファイルの数を数える
             subdirs = os.listdir('Poses/Garbage/')
             index = len(subdirs) + 1
             # Create new example directory
+            #新しいサンプルディレクトリを作成します
             if not os.path.exists('Poses/Garbage/Garbage_' + str(index) + '/'):
                 os.makedirs('Poses/Garbage/Garbage_' + str(index) + '/')
 
                 #Update current path
+                #現在のパスを更新する
                 currentPath = 'Poses/Garbage/Garbage_' + str(index) + '/'
                 currentExample ='Garbage_' + str(index) + '_'
                 name_pose = 'Garbage'
-        
+
 
     print('You\'ll now be prompted to record the pose you want to add. \n \
                 Please place your hand beforehand facing the camera, and press any key when ready. \n \
@@ -86,9 +96,11 @@ def main():
     input()
 
     # Begin capturing
+    #キャプチャを開始します
     cap = cv2.VideoCapture(0)
 
     # Define the codec and create VideoWriter object
+    #コーデックを定義し、VideoWriterオブジェクトを作成します
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     out = cv2.VideoWriter(currentPath + name_pose + '.avi', fourcc, 25.0, (640, 480))
 
@@ -96,6 +108,7 @@ def main():
         ret, frame = cap.read()
         if ret:
             # write the frame
+            #フレームを書く
             out.write(frame)
 
             cv2.imshow('frame', frame)
@@ -105,6 +118,7 @@ def main():
             break
 
     # Release everything if job is finished
+    #ジョブが終了したらすべてを解放します
     cap.release()
     out.release()
     cv2.destroyAllWindows()
@@ -112,6 +126,7 @@ def main():
     vid = cv2.VideoCapture(currentPath + name_pose + '.avi')
 
     # Check if the video
+    #ビデオかどうかを確認します
     if (not vid.isOpened()):
         print('Error opening video stream or file')
         return
@@ -120,31 +135,38 @@ def main():
     detection_graph, sess = detector_utils.load_inference_graph()
     sess = tf.Session(graph=detection_graph)
     print('>> model loaded!')
-    
+
     _iter = 1
     # Read until video is completed
+    #ビデオが完成するまで読む
     while(vid.isOpened()):
         # Capture frame-by-frame
+        #フレームごとにキャプチャする
         ret, frame = vid.read()
         if ret:
             print('   Processing frame: ' + str(_iter))
             # Resize and convert to RGB for NN to work with
+            #NNが機能するようにサイズを変更してRGBに変換する
             frame = cv2.resize(frame, (320, 180), interpolation=cv2.INTER_AREA)
 
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
             # Detect object
+            #オブジェクトを検出する
             boxes, scores = detector_utils.detect_objects(frame, detection_graph, sess)
 
             # get region of interest
+            #関心領域を取得する
             res = detector_utils.get_box_image(1, 0.2, scores, boxes, 320, 180, frame)
 
-            # Save cropped image 
-            if(res is not None):       
+            # Save cropped image
+            #トリミングした画像を保存する
+            if(res is not None):
                 cv2.imwrite(currentPath + currentExample + str(_iter) + '.png', cv2.cvtColor(res, cv2.COLOR_RGB2BGR))
 
             _iter += 1
         # Break the loop
+        #ループを破る
         else:
             break
 
