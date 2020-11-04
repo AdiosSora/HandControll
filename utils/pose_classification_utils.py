e3ab059746c12a736b4bbabc56b9e7b98c83a5e3
S3A1 Student <S3A1@KCS-Learning.local>
2020-11-04 14:00:48 +0900

    pose_classification_utils.py 翻訳

--- utils/pose_classification_utils.py
+++ utils/pose_classification_utils.py
@@ -5,7 +5,9 @@ import tensorflow as tf
 import os; os.environ['KERAS_BACKEND'] = 'tensorflow'
 import keras
 
-def load_KerasGraph(path): 
+#classification
+#分類する
+def load_KerasGraph(path):
     print("> ====== loading Keras model for classification")
     thread_graph = Graph()
     with thread_graph.as_default():
@@ -18,14 +20,18 @@ def load_KerasGraph(path):
     return model, graph, thread_session
 
 def classify(model, graph, sess, im):
+
+    #色変換
     im = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
 
     im = cv2.flip(im, 1)
 
     # Reshape
+    #形を変える
     res = cv2.resize(im, (28,28), interpolation=cv2.INTER_AREA)
 
     # Convert to float values between 0. and 1.
+    #0から1までの浮動小数点値に変換します。
     res = res.astype(dtype="float64")
     res = res / 255
     res = np.reshape(res, (1, 28, 28, 1))
@@ -34,7 +40,7 @@ def classify(model, graph, sess, im):
         with sess.as_default():
             prediction= model.predict(res)
 
-    return prediction[0] 
+    return prediction[0]
 
 def test_classify(model, im):
     im = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
@@ -42,9 +48,11 @@ def test_classify(model, im):
     im = cv2.flip(im, 1)
 
     # Reshape
+    #形を変える
     res = cv2.resize(im, (28,28), interpolation=cv2.INTER_AREA)
 
     # Convert to float values between 0. and 1.
+    #0から1までの浮動小数点値に変換します。
     res = res.astype(dtype="float64")
     res = res / 255
     res = np.reshape(res, (1, 28, 28, 1))
@@ -76,7 +84,7 @@ if __name__ == "__main__":
     print('<< FOUR >>')
     im = cv2.imread("Poses/Four/Four_1/Four_1_867.png")
     print(test_classify(model, im))
-    
+
     # Startrek
     print('<< Startrek >>')
     im = cv2.imread("Poses/Startrek/Startrek_1/Startrek_1_867.png")
