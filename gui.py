b3968988d94143175e7af040080a72e0a683fef2
bata-FM <kotanameko0301@icloud.com>
2020-11-16 14:35:26 +0900

    パーを50回見たら右クリックをするプログラムを追加。 ５０じゃ少し小さすぎるから閾値調整中

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from random import randint
 import autopy
-
+clickCount=0
 def drawInferences(values, names=['', '', '', '', '']):
     nb_classes              = 5
     left_margin             = 150
@@ -22,8 +22,13 @@ def drawInferences(values, names=['', '', '', '', '']):
     for i in range(nb_classes):
         if(values[i] > 0.7):
             cv2.rectangle(blank, (left_margin, margin + int(margin*i)), (left_margin + int(values[i]*200), margin + thickness + int(margin*i)), (0,255,0), -1)
-            #if(values[1] > 0.7):
-                #autopy.mouse.click(autopy.mouse.Button.RIGHT)
+
+            if(values[4] > 0.7):#パーの制度が7割以上識別で１カウントする。→50カウントで右クリックイベントが発火
+                global clickCount
+                clickCount+=1
+                if(clickCount<=50):
+                    autopy.mouse.click(autopy.mouse.Button.RIGHT)
+                    clickCount=0
         else:
             cv2.rectangle(blank, (left_margin, margin + int(margin*i)), (left_margin + int(values[i]*200), margin + thickness + int(margin*i)), (255,255,255), -1)
         cv2.putText(blank, names[i], (0, margin + int(margin*i) + int(thickness/2)), font, fontScale, fontColor, lineType)
