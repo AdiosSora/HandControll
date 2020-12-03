b312d2f99cebf52e11fa7d4b83d3e00c594f043b
kiba-shin <sirugisa@gmail.com>
2020-12-03 11:22:23 +0900

    動作可能に

--- HandPose.py
+++ HandPose.py
@@ -273,7 +273,7 @@ if __name__ == '__main__':
 
         # initialize the folder which contents html,js,css,etc
 
-        hand_gui.start_gui(output_q)
+        # hand_gui.start_gui(output_q)
 
         output_frame = output_q.get()
         cropped_output = cropped_output_q.get()
