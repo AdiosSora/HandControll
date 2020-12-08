156c8ce0a7d0e03d0fea75d5bc342d0da17e6a7e
GABA-alt <noraneko1780@outlook.jp>
2020-12-08 11:42:10 +0900

    カメラIDの受け渡し

--- GUI/index.js
+++ GUI/index.js
@@ -1,19 +1,108 @@
-/*
-不要！！！！！！！！！！！！！！！！！！！！！！
-eel.expose(set_elapsedtime);
-function set_elapsedtime(elapsedtime) {
-      document.getElementById("elapsedtime").innerHTML = "elapsedtime:" + elapsedtime + "s";
+$(document).ready(function(){
+  var select = document.getElementById("selecter");
+  var cnt = 0;
+  navigator.mediaDevices.enumerateDevices()
+  .then(function(devices) {
+    devices.forEach(function(device) {
+      if(device.kind == "videoinput"){
+        var Id = device.deviceId;
+        var Label;
+        if(device.label.indexOf('(') != -1){
+           Label = device.label.slice(0,device.label.indexOf('('));
+           Label = document.createTextNode(String(cnt) + " : " + Label);
+        }else{
+           Label = document.createTextNode(String(cnt) + " : " + device.label);
+        }
+          console.log(device.label);
+          $('#selector').append($('<option>').html(Label).val(Id));
+          cnt += 1;
+
+      }
+    });
+     // エラー発生時
+  }).catch(function(err) {
+    // console.error('カメラの接続に失敗しました', err);
+});
+});
+
+
+//カメラプレビュー
+  var constraints = { audio: false, video: {
+      width: 1920,
+      height: 1080,
+  }}
+
+  navigator.mediaDevices.getUserMedia(constraints)
+    .then(function(stream) {
+      var video = document.querySelector('video');
+      video.srcObject = stream;
+      video.onloadedmetadata = function(e) {
+        video.play();
+      };
+    })
+    .catch(function(err) {
+      console.log(err.name + ": " + err.message);
+    });
+
+//セレクトしたカメラに切り替え
+$(function($) {
+    $('#selector').change(function() {
+        constraints.video.deviceId = $('#selector option:selected').val();
+        video.srcObject.getTracks().forEach(track => track.stop())
+        navigator.mediaDevices.getUserMedia(constraints)
+          .then(function(stream) {
+            const video = document.querySelector('video');
+            video.srcObject = stream;
+            video.onloadedmetadata = function(e) {
+              video.play();
+            };
+          })
+          .catch(function(err) {
+            console.log(err.name + ": " + err.message);
+          });
+    });
+
+});
+
+// // 確認ボタンを押したときにカメラが使用できるか確認
+// $(function($){
+//   $('#check').click(function(){
+//     if(video.srcObject.active == false){
+//       window.alert('カメラの接続を確認してください');
+//     }else{
+//       console.log("seikou");
+//       // window.location.href = 'complete.html';
+//     }
+//   });
+// });
+
+eel.expose(js_function);
+function js_function(){
+  return $('#selector option:selected').text().slice(0,1);
 }
-*/
-    /*
-    eel.expose(set_base64image);
-    function set_base64image(base64image) {
-        document.getElementById("python_video").src = base64image;
-    }
-    */
-/*
-eel.expose(set_base64image2);
-function set_base64image2(base64image2) {
-        document.getElementById("python_video2").src = base64image2;
-}
-*/
+//
+// try{
+//   $(function($){
+//     $('#check').click(function(){
+//       test();
+//     });
+//   });
+// }catch(err){
+//   cons(err);
+// }
+//
+// async function test(){
+//       navigator.mediaDevices.enumerateDevices()
+//       .then(function(devices) {
+//         devices.forEach(function(device) {
+//           if(device.kind == "videoinput"){
+//             var server = device.deviceId;
+//           }
+//         });
+//          // エラー発生時
+//       }).catch(function(err) {
+//         // console.error('カメラの接続に失敗しました', err);
+//     });
+//       let TEST = await eel.test_eel(server);
+//       console.log(TEST);
+// }
--- hand_gui.py
+++ hand_gui.py
@@ -4,35 +4,43 @@ import cv2 as cv
 import time
 import datetime
 
-def start_gui(output_frame, cnt_gui):
-    if(cnt_gui == 0):   #初回時のみにeel.init、eel.start起動、以降起動しない（cnt_guiが1と固定になるため）
-        eel.init('GUI')
-        eel.start(
-        'index.html',
-#        mode='chrome',
-#        cmdline_args=['--start-fullscreen'],
-        block=False)
-        cnt_gui = 1
+def cam_source():
+    eel.init('GUI')
+    eel.start('index.html',block=False)
+    num = eel.js_function()()
+    print(num)
+    return int(num)
 
-#    while True:
-        #start_time = time.time()
 
-    eel.sleep(0.01) #コメントアウトするとindex.htmlにつながらないっぽい
-
-        # カメラキャプチャ #####################################################
-        #ret, frame = cap.read()
-        #if not ret:
-            #continue
-#        output_frame = output_q.get()
-    if(output_frame is not None):
-        # UI側へ転送(画像) #####################################################
-        _, imencode_image = cv.imencode('.jpg', output_frame)
-        base64_image = base64.b64encode(imencode_image)
-        eel.set_base64image("data:image/jpg;base64," +
-                                    base64_image.decode("ascii"))
-
-    return cnt_gui
-
+# def start_gui(output_frame, cnt_gui):
+#     if(cnt_gui == 0):   #初回時のみにeel.init、eel.start起動、以降起動しない（cnt_guiが1と固定になるため）
+#         eel.init('GUI')
+#         eel.start(
+#         'index.html',
+# #        mode='chrome',
+# #        cmdline_args=['--start-fullscreen'],
+#         block=False)
+#         cnt_gui = 1
+#
+# #    while True:
+#         #start_time = time.time()
+#
+#     eel.sleep(0.01) #コメントアウトするとindex.htmlにつながらないっぽい
+#
+#         # カメラキャプチャ #####################################################
+#         #ret, frame = cap.read()
+#         #if not ret:
+#             #continue
+# #        output_frame = output_q.get()
+#     if(output_frame is not None):
+#         # UI側へ転送(画像) #####################################################
+#         _, imencode_image = cv.imencode('.jpg', output_frame)
+#         base64_image = base64.b64encode(imencode_image)
+#         eel.set_base64image("data:image/jpg;base64," +
+#                                     base64_image.decode("ascii"))
+#
+#     return cnt_gui
+#
 #    key = cv.waitKey(1)
 #    if key == 27:  # ESC
 #        break
