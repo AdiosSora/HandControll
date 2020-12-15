430b37175ed6436fe389c41427ff6b2afafa4c0c
rufushyo <lucky_ume3@yahoo.co.jp>
2020-12-15 12:00:29 +0900

    開始時点でwebcam消失の際の動作を追加

--- HandPose.py
+++ HandPose.py
@@ -112,6 +112,7 @@ if __name__ == '__main__':
     flg_video = 0   #「1」でカメラが接続されていない
     flg_break = 0   #「1」で最初のループを抜け終了する⇒正常終了
     flg_restart = 0 #「1」でリスタートした際に hand_gui.py で eel が2度起動するのを防ぐ
+    flg_start = 0   #「1」で開始時点でのカメラ消失
     cnt_gui=0   #hand_guiにてeelを動かす用に使用（0:初回起動時、1:2回目以降起動時、2:カメラが切断された際にhtmlを閉じるために使用）
 
     while(True):    #カメラが再度接続するまでループ処理
@@ -288,7 +289,13 @@ if __name__ == '__main__':
                     #それぞれのフラグを立てて、システムを終了させ、最初の while に戻る
                     flg_video = 1
                     cnt_gui = 2
-                    cnt_gui, flg_end, flg_restart = hand_gui.start_gui(output_frame, cnt_gui, cnt_pose, name_pose, flg_restart)
+                    try:
+                        #webcam が最初から接続されていない場合は except の動作
+                        cnt_gui, flg_end, flg_restart, flg_start = hand_gui.start_gui(output_frame, cnt_gui, cnt_pose, name_pose, flg_restart, flg_start)
+                    except NameError as name_e:
+                        traceback.print_exc()
+                        flg_start = 1
+                        print("webcam接続して！！！！")
                     pool.terminate()
                     video_capture.stop()
                     cv2.destroyAllWindows()
@@ -333,7 +340,7 @@ if __name__ == '__main__':
                 #hand_gui.start_gui(output_frame)
                 output_frame = cv2.cvtColor(output_frame, cv2.COLOR_RGB2BGR)
                 #output_qの内容表示するためにhand_gui.start_guiへ
-                cnt_gui, flg_end, flg_restart = hand_gui.start_gui(output_frame, cnt_gui, cnt_pose, name_pose, flg_restart)
+                cnt_gui, flg_end, flg_restart, flg_start = hand_gui.start_gui(output_frame, cnt_gui, cnt_pose, name_pose, flg_restart, flg_start)
 
                 inferences      = None
 
--- hand_gui.py
+++ hand_gui.py
@@ -12,7 +12,7 @@ def py_sysclose():
     global flg_sys
     flg_sys = 1
 
-def start_gui(output_frame, cnt_gui, cnt_pose, name_pose, flg_restart):
+def start_gui(output_frame, cnt_gui, cnt_pose, name_pose, flg_restart, flg_start):
     if(cnt_gui == 0):   #初回時のみにeel.init、eel.start起動、以降起動しない（cnt_guiが1と固定になるため）
         eel.init('GUI/web/html')
         eel.start(
@@ -24,12 +24,12 @@ def start_gui(output_frame, cnt_gui, cnt_pose, name_pose, flg_restart):
         cnt_gui = 1
     elif(cnt_gui == 2): #カメラが切断された際に Recognize.html を閉じる
         eel.windowclose()
-        return cnt_gui, flg_sys, flg_restart
+        return cnt_gui, flg_sys, flg_restart, flg_start
     #    while True:
             #start_time = time.time()
     try:#Recognize.htmlが × をクリックして終了した場合をキャッチ
         eel.sleep(0.01) #コメントアウトするとindex.htmlにつながらないっぽい
-
+        #flg_restart = 0
             # カメラキャプチャ #####################################################
             #ret, frame = cap.read()
             #if not ret:
@@ -47,7 +47,7 @@ def start_gui(output_frame, cnt_gui, cnt_pose, name_pose, flg_restart):
                                         base64_image.decode("ascii"))
 
         eel.set_posegauge(cnt_pose, name_pose)
-        return cnt_gui, flg_sys, flg_restart
+        return cnt_gui, flg_sys, flg_restart, flg_start
 
     except:# SystemExit as sys_e:#Recognize.htmlが × をクリックして終了した場合の例外処理
         traceback.print_exc()
@@ -55,6 +55,17 @@ def start_gui(output_frame, cnt_gui, cnt_pose, name_pose, flg_restart):
         #再起動した際にすでに eel が起動しているか判定
         if(flg_restart == 1):
             flg_restart = 0
+            if(flg_start == 1):
+                #開始時点でカメラが消失していた場合は、
+                #こちらで eel を再起動し、× をクリックした際の動作を実行
+                flg_start = 0
+                eel.init('GUI/web/html')
+                eel.start(
+                'Recognize.html',
+        #        mode='chrome',
+        #        cmdline_args=['--start-fullscreen'],
+                block=False)
+                print("再起動！！！！")
         else:
             eel.init('GUI/web/html')
             eel.start(
@@ -63,7 +74,7 @@ def start_gui(output_frame, cnt_gui, cnt_pose, name_pose, flg_restart):
     #        cmdline_args=['--start-fullscreen'],
             block=False)
             print("再起動！！！！")
-        return cnt_gui, flg_sys, flg_restart
+        return cnt_gui, flg_sys, flg_restart, flg_start
 
 def cam_source():
     eel.init('GUI/web/html')
